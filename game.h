af3e3daf55e53eb16cd21894e0cfba7697129ba6
Lars Aeschlimann <lars.aeschlimann@students.bfh.ch>
2016-12-21 11:33:09 +0100

    Some changes in game.c and game.h

--- game.h
+++ game.h
@@ -15,16 +15,17 @@
 	#define PLAYER1_POSX	10
 	#define PLAYER1_POSY	10
 	#define PLAYER1_COLOR	GUI_COLOR_BLUE
-	#define PLAYER2_POSX	10
+	#define PLAYER2_POSX	200
 	#define PLAYER2_POSY	10
 	#define PLAYER2_COLOR	GUI_COLOR_RED
 
 	//Ball Einstellungen
-	#define BALL_HEIGHT		10
-	#define	BALL_WIDTH		10
+	#define BALL_RADIUS     10
 	#define BALL_POSX		10
 	#define BALL_POSY		10
 	#define BALL_COLOR		GUI_COLOR_YELLOW
+    #define SPEED_BALL_X    5
+    define  SPEED_BALL_Y    5
 
 	//Prototypen
 	void initGame(void);
